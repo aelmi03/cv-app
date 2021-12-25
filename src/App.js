a75c3acf779bfbf124c53b50a81b52a60dde2821
Abdi-Ladif Elmi <abdiladifelmi03@gmail.com>
2021-12-25 07:55:44 -0800

    Rendered my header componenet into the App componenet and it's styles won't conflict with any other components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
-function App() {
-  return <div className="App"></div>;
+import React, { Component } from "react";
+import Header from "./components/Header/Header";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div>
+        <Header />
+        <h1 className="h1">Hello</h1>
+      </div>
+    );
+  }
 }
 
 export default App;
