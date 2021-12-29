eace93d97067b39159a9f820857d10f131c9c2a3
Abdi-Ladif Elmi <abdiladifelmi03@gmail.com>
2021-12-29 12:04:05 -0800

    Created my GeneralPreview Component which will contain the personal information component and the skills component that will be on the live preview

---
+++ src/components/GeneralPreview/GeneralPreview.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import GeneralPreviewCSS from "./GeneralPreview.module.css";
+
+class GeneralPreview extends Component {
+  render() {
+    return <div className={GeneralPreviewCSS.container}></div>;
+  }
+}
+
+export default GeneralPreview;
--- src/components/LivePreview/LivePreview.js
+++ src/components/LivePreview/LivePreview.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import GeneralPreview from "../GeneralPreview/GeneralPreview";
 import LivePreviewCSS from "./LivePreview.module.css";
 
 class LivePreview extends Component {
@@ -7,6 +8,9 @@ class LivePreview extends Component {
     return (
       <div className={LivePreviewCSS.container}>
         <h1 className={LivePreviewCSS.title}>{`${firstName} ${lastName}`}</h1>
+        <div className={LivePreviewCSS.information}>
+          <GeneralPreview />
+        </div>
       </div>
     );
   }
