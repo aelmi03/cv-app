840a912b9e7db64ecda81895ed783044c2aaaa00
Abdi-Ladif Elmi <abdiladifelmi03@gmail.com>
2021-12-30 12:21:28 -0800

    Finished my DescriptionPreview component which renders a description header, and a description paragraph using the information passed down from the App component

---
+++ src/components/DescriptionPreview/DescriptionPreview.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import PreviewHeading from "../PreviewHeading/PreviewHeading";
+import DescriptionPreviewCSS from "./DescriptionPreview.module.css";
+
+class DescriptionPreview extends Component {
+  render() {
+    return (
+      <div className={DescriptionPreviewCSS.container}>
+        <PreviewHeading title="Description" />
+        <p>{this.props.description}</p>
+      </div>
+    );
+  }
+}
+
+export default DescriptionPreview;
--- src/components/LivePreview/LivePreview.js
+++ src/components/LivePreview/LivePreview.js
@@ -1,19 +1,23 @@
 import React, { Component } from "react";
 import GeneralPreview from "../GeneralPreview/GeneralPreview";
 import SkillsPreview from "../SkillsPreview/SkillsPreview";
+import DescriptionPreview from "../DescriptionPreview/DescriptionPreview";
 import LivePreviewCSS from "./LivePreview.module.css";
 
 class LivePreview extends Component {
   render() {
-    const { generalInfo } = this.props;
-    const { firstName, lastName } = generalInfo;
+    const { generalInfo, skills } = this.props;
+    const { firstName, lastName, description } = generalInfo;
     return (
       <div className={LivePreviewCSS.container}>
         <h1 className={LivePreviewCSS.title}>{`${firstName} ${lastName}`}</h1>
         <div className={LivePreviewCSS.information}>
+          <div className={LivePreviewCSS.mainInfo}>
+            <DescriptionPreview description={description} />
+          </div>
           <div className={LivePreviewCSS.minorInfo}>
             <GeneralPreview generalInfo={generalInfo} />
-            <SkillsPreview skills={this.props.skills} />
+            <SkillsPreview skills={skills} />
           </div>
         </div>
       </div>
