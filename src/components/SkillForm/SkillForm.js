ecc1b6ed3a7221f58ba6ad232aadcd008d739acc
Abdi-Ladif Elmi <abdiladifelmi03@gmail.com>
2022-01-13 23:23:50 -0800

    Changed my SkillInfo and SkillForm components from class components to functional components

--- src/components/SkillForm/SkillForm.js
+++ src/components/SkillForm/SkillForm.js
@@ -1,36 +1,29 @@
-import React, { Component } from "react";
+import React from "react";
 import SkillFormCSS from "./SkillForm.module.css";
 
-class SkillForm extends Component {
-  constructor(props) {
-    super(props);
-    this.updateSkillListener = this.updateSkillListener.bind(this);
-    this.deleteSkillListener = this.deleteSkillListener.bind(this);
-  }
-  updateSkillListener(e) {
-    this.props.updateSkill(this.props.id, e.target.value);
-  }
-  deleteSkillListener() {
-    this.props.deleteSkill(this.props.id);
-  }
-  render() {
-    return (
-      <div className={SkillFormCSS.container}>
-        <form className={SkillFormCSS.form}>
-          <input
-            type="text"
-            className={SkillFormCSS.input}
-            placeholder="Skill"
-            value={this.props.skill}
-            onChange={this.updateSkillListener}
-          />
-        </form>
-        <button className="delete-button" onClick={this.deleteSkillListener}>
-          DELETE
-        </button>
-      </div>
-    );
-  }
-}
+const SkillForm = (props) => {
+  const updateSkillListener = (e) => {
+    props.updateSkill(props.id, e.target.value);
+  };
+  return (
+    <div className={SkillFormCSS.container}>
+      <form className={SkillFormCSS.form}>
+        <input
+          type="text"
+          className={SkillFormCSS.input}
+          placeholder="Skill"
+          value={props.skill}
+          onChange={updateSkillListener}
+        />
+      </form>
+      <button
+        className="delete-button"
+        onClick={props.deleteSkill.bind(this, props.id)}
+      >
+        DELETE
+      </button>
+    </div>
+  );
+};
 
 export default SkillForm;
--- src/components/SkillInfo/SkillInfo.js
+++ src/components/SkillInfo/SkillInfo.js
@@ -1,33 +1,31 @@
-import React, { Component } from "react";
+import React from "react";
 import SkillInfoCSS from "./SkillInfo.module.css";
 import FormHeader from "../FormHeader/FormHeader";
 import SkillForm from "../SkillForm/SkillForm";
 
-class SkillInfo extends Component {
-  loadSkills() {
-    return this.props.skills.map((skillObject) => {
+const SkillInfo = (props) => {
+  const loadSkills = () => {
+    return props.skills.map((skillObject) => {
       return (
         <SkillForm
           skill={skillObject.skill}
           key={skillObject.id}
           id={skillObject.id}
-          updateSkill={this.props.updateSkill}
-          deleteSkill={this.props.deleteSkill}
+          updateSkill={props.updateSkill}
+          deleteSkill={props.deleteSkill}
         />
       );
     });
-  }
-  render() {
-    return (
-      <div className={SkillInfoCSS.container}>
-        <FormHeader title="Skills" />
-        {this.loadSkills()}
-        <button className="add-button" onClick={this.props.addSkill}>
-          ADD
-        </button>
-      </div>
-    );
-  }
-}
+  };
+  return (
+    <div className={SkillInfoCSS.container}>
+      <FormHeader title="Skills" />
+      {loadSkills()}
+      <button className="add-button" onClick={props.addSkill}>
+        ADD
+      </button>
+    </div>
+  );
+};
 
 export default SkillInfo;
